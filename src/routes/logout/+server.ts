52a269f9623f7f35cba22cb58581f63d97311d0e
bokerface <waskitodamar17@gmail.com>
2024-09-14 19:19:17 +0700

    disable logout

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -1,18 +1,18 @@
-import { json, redirect } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import { lucia } from '$lib/server/auth';
+// import { json, redirect } from '@sveltejs/kit';
+// import type { RequestHandler } from './$types';
+// import { lucia } from '$lib/server/auth';
 
-export const POST: RequestHandler = async (event) => {
-	console.log(event.locals.session);
-	if (!event.locals.session) {
-		throw redirect(302, '/login');
-	}
-	await lucia.invalidateSession(event.locals.session.id);
-	const sessionCookie = lucia.createBlankSessionCookie();
-	event.cookies.set(sessionCookie.name, sessionCookie.value, {
-		path: '.',
-		...sessionCookie.attributes
-	});
+// export const POST: RequestHandler = async (event) => {
+// 	console.log(event.locals.session);
+// 	if (!event.locals.session) {
+// 		throw redirect(302, '/login');
+// 	}
+// 	await lucia.invalidateSession(event.locals.session.id);
+// 	const sessionCookie = lucia.createBlankSessionCookie();
+// 	event.cookies.set(sessionCookie.name, sessionCookie.value, {
+// 		path: '.',
+// 		...sessionCookie.attributes
+// 	});
 
-	throw redirect(302, '/login');
-};
+// 	throw redirect(302, '/login');
+// };
